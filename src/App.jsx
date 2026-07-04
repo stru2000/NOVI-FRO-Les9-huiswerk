18cf0396756080a0aace53507f0bfc404f7389d6
stru2000 <robin.strumane@gmail.com>
2026-07-05 00:57:03 +0200

    bonus 1 klaar

--- src/App.jsx
+++ src/App.jsx
@@ -4,53 +4,67 @@ import calculateTotalTvsSold from './helpers/inventory/calculate-sold-tvs.js';
 import calculateTotalTvsPurchased from "./helpers/inventory/calculate-purchased-tvs.js";
 import calculateTotalTvsUnsold from "./helpers/inventory/calculate-unsold-tvs.js";
 import TvCard from "./components/tvCard.jsx";
-import showOutcomeInConsole from './constants/oefenbestand.js';
-import { inventory, bestSellingTv } from './constants/inventory.js';
+// import showOutcomeInConsole from './constants/oefenbestand.js';
+import {inventory, bestSellingTv} from './constants/inventory.js';
 import findMostSold from "./helpers/inventory/find-most-sold.js";
+import {
+    sortByCheapest,
+    sortByLargestScreens,
+    sortByMostSold,
+    sortBySuitableForSports
+} from "./helpers/inventory/sort-inventory.js";
 
 function App() {
 
-const sold = calculateTotalTvsSold();
-const purchased = calculateTotalTvsPurchased();
-const unsold = calculateTotalTvsUnsold();
+    const sold = calculateTotalTvsSold();
+    const purchased = calculateTotalTvsPurchased();
+    const unsold = calculateTotalTvsUnsold();
 
-showOutcomeInConsole();
+    // showOutcomeInConsole();
 
-function handleClick(event) {
-  console.log(event.target.textContent);
-}
+    // function handleClick(event) {
+    //     console.log(event.target.textContent);
+    // }
 
-  return (
-    <>
-      <h1>Tech it easy dashboard</h1>
+    return (
+        <>
+            <h1>Tech it easy dashboard</h1>
 
-      <h2>Verkoopoverzicht</h2>
+            <h2>Verkoopoverzicht</h2>
 
-      <div className="tiles-wrapper">
-        <Tile description="Aantal verkochte producten" amount={sold} name="sold"/>
-        <Tile description="Aantal ingekochte producten" amount={purchased} name="purchased"/>
-        <Tile description="Aantal te verkopen producten" amount={unsold} name="unsold"/>
-      </div>
+            <div className="tiles-wrapper">
+                <Tile description="Aantal verkochte producten" amount={sold} name="sold"/>
+                <Tile description="Aantal ingekochte producten" amount={purchased} name="purchased"/>
+                <Tile description="Aantal te verkopen producten" amount={unsold} name="unsold"/>
+            </div>
 
-      <h2>Best verkochte TV</h2>
+            <h2>Best verkochte TV</h2>
 
-      <TvCard product={findMostSold(inventory)} />
+            <TvCard product={findMostSold(inventory)}/>
 
-      <h2>Alle tvs</h2>
+            <h2>Alle tvs</h2>
 
-      <div className="buttons-wrapper">
-        <button onClick={handleClick}>Meest verkocht eerst</button>
-        <button onClick={handleClick}>Goedkoopste eerst</button>
-        <button onClick={handleClick}>Meest geschikt voor sport eerst</button>
-      </div>
+            <div className="buttons-wrapper">
+                <button onClick={()=>{sortByMostSold(inventory)}}>Meest verkocht eerst</button>
+                <button onClick={()=>{sortByCheapest(inventory)}}>Goedkoopste eerst</button>
+                <button onClick={()=>{sortBySuitableForSports(inventory)}}>Meest geschikt voor sport eerst</button>
+                <button onClick={()=>{sortByLargestScreens(inventory)}}>Grootste schermgrootte eerst</button>
+            </div>
 
-    <ul>
-        {inventory.map((tv) => {
-            return <li key={tv.type}>{tv.brand}</li>;
-        })}
-    </ul>
-    </>
-  )
+            {/*<ul>*/}
+            {/*    {inventory.map((tv) => {*/}
+            {/*        return <li key={tv.type}>{tv.brand}</li>;*/}
+            {/*    })}*/}
+            {/*</ul>*/}
+
+            <ul>
+                {inventory.map((tv) => {
+                    return <TvCard key={tv.type} product={tv}/>;
+                })}
+            </ul>
+
+        </>
+    )
 }
 
 export default App
--- src/constants/oefenbestand.js
+++ src/constants/oefenbestand.js
@@ -2,13 +2,13 @@ import { inventory } from './inventory.js';
 
 function showOutcomeInConsole() {
     const tvTypes = inventory.map((tv)=> tv.type );
-    console.log(tvTypes);
+    console.log(`TV Types:`,tvTypes);
 
     const soldOut = inventory.filter((tv)=> tv.originalStock - tv.sold === 0);
-    console.log(soldOut);
+    console.log(`Uitverkocht:`,soldOut);
 
     const specModel = inventory.find((tv)=> tv.type === 'NH3216SMART');
-    console.log(specModel);
+    console.log(`Specs model NH3216SMART:`,specModel);
 
     const sportModelArray = inventory.map((tv)=> {
         return {
@@ -16,19 +16,29 @@ function showOutcomeInConsole() {
             suitable: tv.refreshRate >= 100
         }
     });
-    console.log(sportModelArray);
+    console.log(`Geschikt om sport te kijken:`,sportModelArray);
 
 
     const wideModel = inventory.filter ((tv) =>
         tv.availableSizes.find((size)=> size  >= 65 ))
-    console.log(wideModel);
+    console.log(`65 Inch of meer:`,wideModel);
 
     const featureAmbiLight = inventory.filter ((tv) =>
         tv.options.find((feature) => feature.name === 'ambiLight' && feature.applicable ===true ));
-    console.log(featureAmbiLight)
+    console.log(`Heeft AmbiLight:`,featureAmbiLight)
 
     const mostSold = inventory.sort ((a,b) => b.sold - a.sold);
-    console.log(mostSold[0]);
+    console.log(`Best verkochte toestel:`,mostSold[0]);
+
+    const largestScreen = inventory.map((tv) =>
+        tv.availableSizes.sort((a,b)=> b - a)[0]);
+    console.log(`Grootste scherm:`,largestScreen);
+
+    //niet fool proof - niet gebruiken
+    const largestScreenB = inventory.map((tv) =>
+        tv.availableSizes[tv.availableSizes.length-1]);
+    console.log(`Grootste scherm anders:`,largestScreenB);
+
 
 }
 
